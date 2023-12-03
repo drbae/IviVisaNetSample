22393edb77bf17421b774de15b52f0d95250bdd7
DrBAE <amadeus.bae@gmail.com>
2023-12-04 01:05:59 +0900

    test

--- VisaNetSample/Program.cs
+++ VisaNetSample/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Ivi.Visa;
-using Keysight.Visa;
+//using Keysight.Visa;
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -9,16 +9,18 @@ internal class Program
     static void log(object? msg, [CallerMemberName] string? caller = "") 
         => Console.WriteLine($"{caller}(): {msg}");
 
-    static (AssemblyName gmAssembly, string? implVersion) CheckVisaImplementationInstalled()
+    static (string? name, Version? version, string? implVersion) CheckVisaImplementationInstalled()
     {
-        return (getIviVisaAssemblyName(), getVisaImplVersion());
+        var visa = getIviVisaAssemblyName();
+        return (visa.name, visa.version, getVisaImplVersion());
 
-        AssemblyName getIviVisaAssemblyName()
+        (string name, Version version) getIviVisaAssemblyName()
         {
             try
             {
                 // Get a VISA.NET library version.
-                return typeof(GlobalResourceManager).Assembly.GetName();
+                var name = typeof(GlobalResourceManager).Assembly.GetName();
+                return (name.Name!, name.Version!);
                 //log($"{name.Name} : version={name.Version}.");
             }
             catch (TypeInitializationException ex) when (ex.InnerException is DllNotFoundException)
@@ -38,17 +40,14 @@ internal class Program
             {
                 var vi = FileVersionInfo.GetVersionInfo(Path.Combine(Environment.SystemDirectory, VISA_CONFMGR_DLL));
                 return vi.ProductVersion ?? $"{vi.ProductMajorPart}.{vi.ProductMinorPart}";
-                //log($"VISA Shared Components version {vi.ProductVersion} detected.");
             }
             catch (FileNotFoundException) { throw new VisaImplNotFoundException(); }
-            catch (VisaException ex) when (ex.Message == "No vendor-specific VISA .NET implementation is installed.")
-            { throw new VisaImplNotFoundException(); }
         }
     }
 
     static void Main(string[] args)
     {
-        (AssemblyName gmAssembly, string? implVersion)? _visaInfo = null;
+        (string? name, Version? version, string? implVersion)? _visaInfo = null;
 
         try
         {
@@ -56,14 +55,12 @@ internal class Program
             log($"_visaInfo={_visaInfo}");
 
             //var names = GlobalResourceManager.Find();
-            using var rm = new ResourceManager();
-            var rns = rm.Find("?*");
-
             var rn = "TCPIP0::127.0.0.1::29979::SOCKET";
-            using var device = rm.Open(rn, AccessModes.ExclusiveLock, 100);
-            log($"rn={rn}");
+            //using var rm = new Keysight.Visa.ResourceManager();
+            //using var device = rm.Open(rn, AccessModes.ExclusiveLock, 100);
+            using var device = GlobalResourceManager.Open(rn, AccessModes.ExclusiveLock, 10000);
             log($"device={device.ResourceName}");
-            //using var device = GlobalResourceManager.Open(rn, AccessModes.ExclusiveLock, 2000);
+
             if (device is IMessageBasedSession session)
             {
                 // Ensure termination character is enabled as here in example we use a SOCKET connection.
@@ -86,11 +83,15 @@ internal class Program
             log($"VISA implementation compatible with VISA.NET Shared Components not found."
                 + " Please install corresponding vendor-specific VISA implementation first.");
         }
+        catch (VisaException ex) when (ex.Message == "No vendor-specific VISA .NET implementation is installed.")
+        { 
+            throw new VisaImplNotFoundException(); 
+        }
         catch (EntryPointNotFoundException)
         {
             // Installed VISA Shared Components is not compatible with VISA.NET Shared Components
             log($"Installed VISA Shared Components version {_visaInfo?.implVersion} does not support"
-                + $" VISA.NET {_visaInfo?.gmAssembly.Version}. Please upgrade VISA implementation.");
+                + $" VISA.NET {_visaInfo?.version}. Please upgrade VISA implementation.");
         }
         catch (Exception ex)
         {
